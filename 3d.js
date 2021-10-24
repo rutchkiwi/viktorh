d59ce496beb721174fbb8857b9b6203fd8f7f2a2
Viktor <viktor@medino.com>
2021-10-24 20:54:33 +0100

    zoom in

--- 3d.js
+++ 3d.js
@@ -18,22 +18,28 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.DodecahedronGeometry(12);
+const geometry = new THREE.DodecahedronGeometry(1);
 const material = new THREE.MeshBasicMaterial({wireframe: true});
-const shape = new THREE.Mesh(geometry, material);
+const object = new THREE.Mesh(geometry, material);
 
-shape.rotation.x = Math.random();
-shape.rotation.y = Math.random();
-shape.rotation.z = Math.random();
+object.rotation.x = Math.random();
+object.rotation.y = Math.random();
+object.rotation.z = Math.random();
 
-scene.add(shape);
+scene.add(object);
 
 const speed = 0.4;
+const zoomSpeed = 0.5;
 function animate(){
 	requestAnimationFrame(animate);
-	shape.rotation.x += speed * 0.0008;
-	shape.rotation.y += speed * 0.0007;
-	shape.rotation.z -= speed * 0.0011;
+	if(object.scale.x < 11) {
+		object.scale.x += zoomSpeed;
+		object.scale.y += zoomSpeed;
+		object.scale.z += zoomSpeed;
+	}
+	object.rotation.x += speed * 0.0008;
+	object.rotation.y += speed * 0.0007;
+	object.rotation.z -= speed * 0.0011;
 
 	const color = new THREE.Color(
 		getComputedStyle(document.body).getPropertyValue('--line-color'));
